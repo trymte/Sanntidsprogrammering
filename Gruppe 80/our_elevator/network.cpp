c4e9afd408ae65bf6432897be0516111db42fa5f
Trym Tengesdal <tengesdal1994@gmail.com>
2017-02-11 15:59:43 +0100

    updated network files

--- Gruppe 80/our_elevator/network.cpp
+++ Gruppe 80/our_elevator/network.cpp
@@ -2,9 +2,39 @@
 
 Network::Network(int n_elevators){
 	this->n_elevators = n_elevators;
-	Elevator elev;
-	
+	Status temp = {0,0,"",false};
+	Elevator elev_temp(temp);
 	for(int i = 0; i < this->n_elevators ; i++){
-		elevators = 
+		elevators[i] = elev_temp;
 	}
+}
+
+void Network::nw_message_recieve(string message_id, int elevator_number){
+	switch(message_id){
+		case "supervisor_informed":
+
+
+			break;
+		case "new_order_matrix":
+
+
+			break;
+		case "slave_order_complete":
+			sv_manage_completed_order(elevators[elevator_number]);
+			break;
+		case "slave_order_incomplete":
+			sv_manage_incompleted_order(elevators[elevator_number]);
+			break;
+		case "slave_request_order_matrix":
+			nw_distribute_order_matrix(Queue_element &order_matrix);
+			break;
+		default:
+			break
+
+	}
+
+}
+
+void Network::nw_inform_supervisor(Elevator &elevator){
+
 }
--- Gruppe 80/our_elevator/network.h
+++ Gruppe 80/our_elevator/network.h
@@ -15,16 +15,24 @@ private:
 	int n_elevators;
 	Elevator elevators[n_elevators];
 	Elevator_online elevators_online[n_elevators];
+	void nw_messagestring_to_object(string message);
 public:
 	Network(int n_elevators);
-	void nw_message_recieve();
-	void nw_inform_supervisor(Elevator *elevator);
-	void nw_slave_request_order_matrix();
-	void nw_distribute_order_matrix(Queue_element &order_matrix_ptr);
-	Elevator* nw_get_elevators();
-	void nw_slave_order_complete(Elevator elevator);
-	void nw_slave_order_incomplete(Elevator elevator);
-	bool nw_ping_elevator(Elevator elevator);
 
+	void nw_message_recieve();
+
+	void nw_inform_supervisor(Elevator &elevator);
+
+	void nw_slave_request_order_matrix();
+
+	void nw_distribute_order_matrix(Queue_element &order_matrix_ptr);
+
+	Elevator* nw_get_elevators(){return elevators;}
+
+	void nw_slave_order_complete(Elevator &elevator);
+
+	void nw_slave_order_incomplete(Elevator &elevator);
+
+	bool nw_ping_elevator(Elevator &elevator);
 
 };
