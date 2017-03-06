ac434dff1b388454179ca40e9629d884f8e30a9e
Student <student@itk-d650>
2017-03-06 19:05:15 +0100

    small change, changed const_struct_def to utilities

--- Gruppe 80/our_elevator/main_network_testing.cpp
+++ Gruppe 80/our_elevator/main_network_testing.cpp
@@ -1,6 +1,4 @@
 //TEST MAIN FOR Å PRØVE Å SENDE MELDING OVER NETTVERK
-#include "utilities.h"
-#include "queue.h"
 #include "network.h"
 #include <stdio.h>
 #include <thread>
@@ -42,27 +40,6 @@ void network_listen(Elevator* my_elevator, Network &my_network, Queue& my_queue)
 	}
 }
 */
-
-void network_main(Elevator* my_elevator, Network &my_network, Queue &my_queue){
-	while(1){
-		switch(my_elevator->get_elevator_role()){
-			case MASTER:
-				usleep(250000);
-				//std::cout << "master send via broadcast" << std::endl;
-				//my_network.send_message_packet(MASTER_DISTRIBUTE_ORDER_MATRIX, my_elevator->get_elevator_ID());
-				//std::cout << "master recieve via reciever" << std::endl;
-				my_network.recieve_message_packet(my_elevator->get_elevator_ID());
-				break;
-			case SLAVE:
-				usleep(250000);
-				std::cout << "slave Send via send" << std::endl;
-				my_network.send_message_packet(SLAVE_REQUEST_ORDER_MATRIX, my_elevator->get_elevator_ID());
-				//std::cout << "slave Recieve via recieve broadcast" << std::endl;
-				my_network.recieve_message_packet(my_elevator->get_elevator_ID());
-				break;
-		}
-	}
-}
 
 int main(){
 	
