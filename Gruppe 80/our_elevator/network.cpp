ab77691edba9ec63d5c6ab57e87576511e45b2cd
trt <trt@itk-d650>
2017-03-11 10:49:44 +0100

    awdwa

--- Gruppe 80/our_elevator/network.cpp
+++ Gruppe 80/our_elevator/network.cpp
@@ -272,7 +272,7 @@ void Network::send_message_packet(Message message, int this_elevator_ID, std::st
 
 bool Network::is_node_responding(int this_elevator_ID, int foreign_elevator_ID){
 	struct code_message code;
-	std::endl;
+	std::cout << std::endl;
 	std::cout << "------------------------------------------------------------------------"<< std::endl;
 	std::cout << "Send HANDSHAKE, w8 for response: ";
 	std::cout << "my ip:  " << this->elevators[this_elevator_ID]->get_elevator_ip() << std::endl;
@@ -333,7 +333,7 @@ void network_communication(Elevator* my_elevator, Network &my_network){
 				my_network.recieve_message_packet(my_elevator->get_elevator_ID());
 				break;
 			case SLAVE:
-				my_network.send_message_packet(SLAVE_IP_INIT, my_elevator->get_elevator_ip(), my_network.get_master_ip());
+				my_network.send_message_packet(SLAVE_IP_INIT, my_elevator->get_elevator_ID(), my_network.get_master_ip());
 				my_network.recieve_message_packet(my_elevator->get_elevator_ID());
 				break;
 		}
