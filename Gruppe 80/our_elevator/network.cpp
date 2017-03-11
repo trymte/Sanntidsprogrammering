c673a0ce9e184c61eded1eab255674d6cf35fa89
trt <trt@itk-d650>
2017-03-11 12:42:16 +0100

    dwiajfa

--- Gruppe 80/our_elevator/network.cpp
+++ Gruppe 80/our_elevator/network.cpp
@@ -358,10 +358,5 @@ void network_ping(Elevator* my_elevator, Network &my_network){
 		my_network.check_responding_elevators(my_elevator->get_elevator_ID());
 		my_network.check_my_role(my_elevator->get_elevator_ID());
 		my_network.recieve_handshake_message(my_elevator->get_elevator_ID());
-		switch(my_elevator->get_elevator_role()){
-			case MASTER:
-				my_network.send_message_packet(MASTER_DISTRIBUTE_ORDER_MATRIX, my_elevator->get_elevator_ID(),"");
-				break;
-		}
 	}
 }
